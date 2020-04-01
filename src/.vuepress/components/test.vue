aa01ad0d34516f39b2c22af19a2f0cc9b7091121
松田淳司 <amatsuda@zaico.co.jp>
2020-04-01 23:31:24 +0900

    Fix: 色を調整

--- src/.vuepress/components/test.vue
+++ src/.vuepress/components/test.vue
@@ -21,7 +21,8 @@
             }).then((response) => {
                 const rare_data = response.data
                 let replaced_data = rare_data.replace(/href=\"\/ou\//g, 'href="/www.pref.yamagata.jp/ou/')
-                // replaced_data = replaced_data.replace(/src=\"\/ou\//g, 'src="/www.pref.yamagata.jp/ou/')
+                replaced_data = replaced_data.replace(/src=\"\/ou\//g, 'src="/www.pref.yamagata.jp/ou/')
+                replaced_data = replaced_data.replace(/color=\"#0066cc\"/g, '')
                 this.test = replaced_data
             })
         }
@@ -30,7 +31,11 @@
 
 <style>
     /* 調整用 */
-    .invisible, #header, .pankuzu {
+    .invisible, #header, .pankuzu, .skiptranslate, #footer, #toiawase, .pagetop, #right-side {
         display: none;
+    }
+
+    a[name] {
+        color: #2c3e50;
     }
 </style>
