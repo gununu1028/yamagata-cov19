504e5ed432f7c43a8d19b51b7c32ab873d921021
松田淳司 <amatsuda@zaico.co.jp>
2020-04-01 22:10:27 +0900

    Fix: 取得したHTMLを表示できるように

---
+++ src/.vuepress/components/test.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="component_root">
+        <div v-html="test"></div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "test",
+        data() {
+            return {
+                test: ''
+            }
+        },
+        mounted() {
+            axios({
+                method: 'GET',
+                url: '/test.html',
+            }).then((response) => {
+                this.test = response.data
+            })
+        }
+    }
+</script>
+
+<style>
+    .invisible, #header, .pankuzu {
+        display: none;
+    }
+</style>
