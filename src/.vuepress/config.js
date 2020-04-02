e1fdb4ae117b5d645d8deb97a583a8701be004dc
松田淳司 <amatsuda@zaico.co.jp>
2020-04-02 23:05:08 +0900

    Fix: タイトルなどを見直し

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     base: "/",
     dest: process.cwd() + "/dest/",
-    title: "コロナウイルス",
+    title: "新型コロナウイルス感染症 山形県",
+    description: "このページは有志によって作成されているものです。 最新の情報は山形県ウェブサイトをご覧ください。",
     locales: {
         "/": {
             lang: "ja"
